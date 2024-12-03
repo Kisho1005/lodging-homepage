a8b1bffd0ef4bbbfefdf2f37b5691507b3ad6052
kisyo1005 <kisyo15@i.softbank.jp>
2024-12-03 18:59:52 +0900

    feat: create map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Facilities from "./pages/facilities/Facilities";
 import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
 import "./App.css";
 
 
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/facilities" element={<Facilities />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Header />
+    //   <Routes>
+    //     <Route path="/Home" element={<Home />} />
+    //     <Route path="/facilities" element={<Facilities />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <Home/>
   );
-};
+}
 
 export default App;
